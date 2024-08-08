9f06f0a33ff331e2085a5315222b469f90bb2b4f
Sindiso <mkhsin035@myuct.ac.za>
2024-08-08 17:04:27 +0200

    Added Logger class to encapsulate all log-related functions

--- src/util.cpp
+++ src/util.cpp
@@ -14,3 +14,81 @@ std::minstd_rand randint;
 std::mt19937 engine(rnd());
 std::uniform_real_distribution<double> disRandom(0.0, 1.0);
 std::normal_distribution<> disNormal(0,1);
+
+std::string getCurrentTimeAsReadableString(){
+	// --- get current time information
+
+	time_t now = time(NULL);
+	char timestamp[19] = "";
+	//strftime (timestamp, 19,"%Y%m%d-%Hh%Mm%Ss", localtime(&now));
+	strftime (timestamp, 19,"%Y%m%d-%Hh%Mm%Ss", localtime(&now));
+	std::string s = timestamp;
+
+	// --- get milliseconds resolution (note: could be used to replace code block above - left for tutorial)
+
+	struct timeval now2;
+    int mtime;
+    gettimeofday(&now2, NULL);
+
+	mtime = now2.tv_usec;
+	if ( mtime < 100000 )
+	{
+		s+="0";
+		if ( mtime < 10000 )
+			s+="0";
+		if ( mtime < 1000 )
+			s+="0";
+		if ( mtime < 100 )
+			s+="0";
+		if ( mtime < 10 )
+			s+="0";
+	}
+	s += convertToString(mtime) + "us"; // microseconds
+
+	return s;
+
+}
+std::string getpidAsReadableString(){
+    return boost::lexical_cast<std::string>((long)::getpid());
+}
+std::string convertToString( int __value ){
+	std::string s;
+	std::stringstream sOutTmp;
+	sOutTmp << __value;
+	s = sOutTmp.str();
+
+	return s;
+}
+/*****************************************************
+ * Logger class definition
+ ****************************************************/
+Logger::Logger(std::ofstream __logFile){
+    logFile = &__logFile;
+	buffer = "";
+}
+
+Logger::Logger(){
+    logFile = &gLogFile;
+}
+
+Logger::~Logger(){}
+
+void Logger::write(std::string str){
+    buffer += str;
+}
+
+void Logger::flush(){
+    if ( ! buffer.empty() )
+    {
+        (*logFile) << buffer;
+        buffer.clear();
+    }
+}
+
+Logger* Logger::make_DefaultLogger(){
+    return new Logger();
+}
+
+void Logger::setLoggerFile ( std::ofstream &__logFile ){
+    logFile = &__logFile;
+}
--- src/util.h
+++ src/util.h
@@ -15,9 +15,51 @@
 
 #include <cstdlib> // RAND_MAX
 #include <random>
+#include <chrono>
+#include <sys/time.h>
+#include <fstream>
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+class Logger;
 /**
  * \brief GLOBAL VARIABLES
  */
+
+/***************************************
+ * Default logging
+ **************************************/
+/**
+ * \brief Log directory name
+ */
+extern std::string gLogDirectoryname;
+/**
+ * \brief Log file name
+ */
+extern std::string gLogFilename;
+/**
+ * \brief Log full filename - directory
+ */
+extern std::string gLogFullFilename;
+/**
+ * \brief Log file
+ */
+extern std::ofstream gLogFile;
+/**
+ * \brief Logger
+ */
+extern Logger *gLogger;
+
+/****************************************
+ * Specified logging
+ ***************************************/
+/**
+ * \brief Robot state (commitment and opinion) log file
+ */
+static std::ofstream gRobotStateLogFile;
+/**
+ * \brief Behavior map logger
+ */
+static Logger* gMapsLogger;
 
 /**
  *\brief Random seed. Default value (="-1") means time based.
@@ -57,5 +99,100 @@ extern std::uniform_int_distribution<int> randomSensor;
  * \brief Normal distribution mean=0 and stddev=1 (use: mean+rand*stddev)
  */
 #define randgaussian() disNormal(engine)
+
+/**
+ * \brief Return current time in a string, with readable format - e.g. 20100526-10h12m08s
+ *
+ * check: http://www.cplusplus.com/reference/clibrary/ctime/strftime/
+ */
+std::string getCurrentTimeAsReadableString();
+
+/** \brief Return PID as a string. Useful to desambiguate timestamps */
+std::string getpidAsReadableString();
+
+/**
+ * \brief Convert integer to string. check: http://notfaq.wordpress.com/2006/08/30/c-convert-int-to-string/
+ * 	@param int value
+ * 	@return std::string string representation
+ */
+std::string convertToString( int __value );
+
+/*
+
+ How to use Log?
+
+ OPTION 1:
+    with any new instance of roborobo, a gLogManager is created and available. You just have to use the two following commands:
+    (1) gLogManager.write()
+    (2) optionally: gLogManager.flush() -- note that this command is called automatically after each evaluation.
+
+ OPTION 2:
+    You can create as many new LogManager as you want. By default they will write in the same file as gLogManager. Here is how to change this:
+
+        std::string filename = "logs/test.txt";
+        std::ofstream file;
+
+        file.open(filename.c_str());
+
+        if(!file) {
+            std::cout << "[error] Cannot open log file " << std::endl;
+            exit (-1);
+        }
+
+        LogManager *lm = new LogManager();
+
+        lm->setLogFile(file);
+
+        lm->write("all work and no play makes Jack a dull boy ");
+
+        lm->flush();
+
+        file.close();
+
+    Some remarks on option 2:
+        (1) dont forget to reassign file target (cf. lm->setLogFile(...) )
+        (2) dont forget to flush once in a while (no flush means information is not written on disk)
+ */
+
+/**
+ * \brief Class to handle logging
+ */
+class Logger {
+
+private:
+
+	std::string buffer;
+    std::ofstream *logFile;  // LogManager does not open/close. Assume it is handled elsewhere.
+
+public:
+    /**
+     * \brief Default Constructor
+     */
+    Logger();
+    /**
+     * \brief Constructor
+     */
+    Logger(std::ofstream __logFile);
+    /**
+     * \brief Destructor
+     */
+	virtual ~Logger();
+	/**
+	 * \brief Sets logging to defaul logger
+	 */
+    static Logger* make_DefaultLogger(); //(std::ofstream __logFile);
+    /**
+     * \brief Sets logger file
+     */
+    void setLoggerFile ( std::ofstream &__logFile );
+    /**
+     * \brief Writes to designated logfile
+     */
+	void write(std::string str);
+	/**
+	 * \brief Saves last written info
+	 */
+    void flush();
+};
 
 #endif /* UTIL_H_ */
