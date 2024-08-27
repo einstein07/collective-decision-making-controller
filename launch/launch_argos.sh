a913dbbee9601053cd084899bc4343d0d36ecf93
Sindiso <mkhsin035@myuct.ac.za>
2024-08-27 17:22:13 +0200

    Update of argos parameters for logging purposes

--- launch/launch_argos.sh
+++ launch/launch_argos.sh
@@ -2,8 +2,8 @@
 
 # The purpose of this script is to set up the necessary environment variables for argos,
 # anf then spin off the argos simulator.
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME//ros2_ws/install/collective_decision_making/lib
-export ARGOS_PLUGIN_PATH=$HOME/ros2_ws/install/collective_decision_making/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME//ros2_ws/install/loop_functions/lib
+export ARGOS_PLUGIN_PATH=$HOME/ros2_ws/install/loop_functions/lib/
 
 argos3 -c world.argos
 
